7762ca57a708fc2edd5f324e877aa3b00e56f71c
turn814 <00814isaac@gmail.com>
2026-04-07 07:26:44 -0700

    uart_tx

--- usb_project/uart_tx/top.v
+++ usb_project/uart_tx/top.v
@@ -1,29 +1,74 @@
 module top (
-    input clk;
-    input btn1;
-    output reg uart_tx;
+    input clk,
+    input btn1,
+    output reg uart_tx
 );
 
-parameter STATE_IDLE    = 2'b00;
-parameter STATE_START   = 2'b01;
-parameter STATE_DATA    = 2'b10;
-parameter STATE_STOP    = 2'b11;
+parameter STATE_IDLE    = 3'b000;
+parameter STATE_START   = 3'b001;
+parameter STATE_DATA    = 3'b010;
+parameter STATE_STOP    = 3'b011;
+parameter STATE_WAIT    = 3'b100;
 
-reg [1:0] current_state = STATE_IDLE;
+reg [2:0] current_state = STATE_IDLE;
 reg [7:0] baud_counter = 0;
-reg [2:0] bit_index = 0;
+reg [4:0] bit_index = 0;
+reg [7:0] tx_data = 8'h41;
 
-initial uart_tx = 1'b1;
+initial uart_tx <= 1'b1;
 
 always @(posedge clk) begin
     case (current_state)
         STATE_IDLE: begin
+            uart_tx <= 1'b1;
+            baud_counter <= 0;
+            bit_index <= 0;
             if (btn1 == 1'b0) begin
                 current_state <= STATE_START;
+            end
         end
 
         STATE_START: begin
-            
+            uart_tx <= 1'b0;
+            if (baud_counter < 104) begin
+                baud_counter <= baud_counter + 1;
+            end else begin
+                baud_counter <= 0;
+                current_state <= STATE_DATA;
+            end
+        end
+
+        STATE_DATA: begin
+            if (bit_index < 8) begin
+                if (baud_counter < 104) begin
+                    uart_tx <= tx_data[bit_index];
+                    baud_counter <= baud_counter + 1;
+                end else begin
+                    bit_index <= bit_index + 1;
+                    baud_counter <= 0;
+                end
+            end else begin
+                baud_counter <= 0;
+                bit_index <= 0;
+                current_state <= STATE_STOP;
+            end
+        end
+
+        STATE_STOP: begin
+            uart_tx <= 1'b1;
+            if (baud_counter < 104) begin
+                baud_counter <= baud_counter + 1;
+            end else begin
+                baud_counter <= 0;
+                current_state <= STATE_WAIT;
+            end
+        end
+
+        STATE_WAIT: begin
+            uart_tx <= 1'b1;
+            if (btn1 == 1'b1) begin
+                current_state <= STATE_IDLE;
+            end
         end
     endcase
 end
